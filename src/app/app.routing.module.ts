91f724aa23b018b9e0585556bc4aaf66ac9a1fdf
João Vitor <joaovitor.jsr@gmail.com>
2021-08-31 03:12:28 -0300

    new

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     FilmesModule
   ],
   exports: [RouterModule]
