43f1682b2a67780c303a9eb6eddeacd7a2a1a337
João Vitor <joaovitor.jsr@gmail.com>
2021-09-01 16:18:23 -0300

    new

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = 'curso-angular';
+const nomeApp = 'Watch';
 const app = express();
+
 
 app.use(express.static(`${__dirname}/dist/${nomeApp}`));
 
